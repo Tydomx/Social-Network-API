c13ba4d4871119ac3ac13afecfa44bcaa8b832d0
Tydomx <michaelthai16@gmail.com>
2022-08-02 15:49:04 -0500

    Added CRUD routes for user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -16,6 +16,58 @@ const UserController = {
 				console.log(err);
 				res.status(400).json(err);
 			});
+	},
+	// GET One user by id
+	getUserById({ params }, res) {
+		User.findOne({ _id: params.id })
+			.populate({
+				path: 'thoughts',
+				select: '-__v'
+			})
+			.select('-__v')
+			.then(dbUserData => {
+				// if no user found, send 404
+				if (!dbUserData) {
+					res.status(404).json({ message: 'No user found with this id!' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch(err => {
+				console.log(err);
+				res.status(400).json(err);
+			});
+	},
+	// POST create user
+	createUser({ body }, res) {
+		User.create(body)
+			.then(dbUserData => res.json(dbUserData))
+			.catch(err => res.status(400).json(err));
+	},
+	// PUT update user
+	updateUser({ params, body }, res) {
+		User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+			.then(dbUserData => {
+				if (!dbUserData) {
+					res.status(404).json({ message: 'No user found with this id!!' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch(err => res.status(400).json(err));
+	},
+
+	// DELETE route /api/users/:id
+	deleteUser({ params }, res) {
+		User.findOneAndDelete({ _id: params.id })
+			.then(dbUserData => {
+				if (!dbUserData) {
+					res.status(404).json({ message: 'No user found with this id to delete!' });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch(err => res.status(400).json(err));
 	}
 };
 
