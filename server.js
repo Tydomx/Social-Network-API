774d2ef98d9e1a68e2760810a8546f43dbbdd443
Tydomx <michaelthai16@gmail.com>
2022-08-02 15:17:25 -0500

    Added server connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+// importing mongoose
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+// tells mongoose which DB we want to connect to. If env exists it'll use that.
+// otherwise go to the local DB link
+mongoose.connect(process.env.MONGODB_URI || 'mongodb:localhost/social-network-api', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+
+// use this to log mongo queries that're being executed
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected to localhost:${PORT}`));
